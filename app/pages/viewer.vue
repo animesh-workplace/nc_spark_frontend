77a38de5503e264c5027741aa8022e45874fe764
Animesh <animesh.workplace@gmail.com>
2026-03-06 15:30:14 +0530

    added file - app/pages/viewer.vue

---
+++ app/pages/viewer.vue
@@ -0,0 +1,13 @@
+<!-- app/pages/genome.vue -->
+<template>
+	<div class="p-4">
+		<h1 class="text-xl font-bold mb-4">hg19 Genome Browser</h1>
+		<ClientOnly fallback-tag="p" fallback="Loading HiGlass...">
+			<HiGlassViewer />
+		</ClientOnly>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
