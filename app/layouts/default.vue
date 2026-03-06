a35dd8f44cba68539e9a6a5b4376289c75a7937f
Animesh <animesh.workplace@gmail.com>
2026-03-06 15:30:14 +0530

    added file - app/layouts/default.vue

---
+++ app/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="font-sans">
+		<NavBar />
+		<main>
+			<slot />
+		</main>
+		<!-- <Footer /> -->
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
