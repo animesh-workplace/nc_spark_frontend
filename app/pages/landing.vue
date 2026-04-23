733524cb08fa5df49f17b8eb221f4eb0048f9af9
Animesh <animesh.workplace@gmail.com>
2026-04-23 23:05:56 +0530

    added file - app/pages/landing.vue

---
+++ app/pages/landing.vue
@@ -0,0 +1,14 @@
+<template>
+	<main>
+		<LandingSection1 />
+		<LandingSection2 />
+		<LandingSection3 />
+		<LandingSection4 />
+		<LandingSection5 />
+		<LandingSection6 />
+	</main>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
