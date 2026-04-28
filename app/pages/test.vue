3c32f9b0d1be2f6e45d0443e2223fcfbb86c4a8b
Animesh <animesh.workplace@gmail.com>
2026-04-28 11:00:10 +0530

    updated file - app/pages/test.vue

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,6 +1,7 @@
 <template>
 	<section class="py-20 min-h-screen bg-stone-200 px-5 md:px-8">
-		<DashboardDistribution />
+		<DashboardDistribution class="mb-10" />
+		<DashboardChrDistribution />
 	</section>
 </template>
 
